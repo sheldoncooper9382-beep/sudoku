1e86d48ed89401445fd7316ca69432e2b2955b65
Freetoolhub <sheldoncooper9382@gmail.com>
2026-01-11 13:51:25 -0500

    Update levelPage.js

--- levelPage.js
+++ levelPage.js
@@ -1,18 +1,3 @@
-var myLevel;
-
-function SetLevel(level){
-
-    if (level=="easyLevel"){
-      myLevel="board1"
-    }else if(level=="mediumLevel"){
-      myLevel="board2"
-    }else{
-      myLevel="board3"
-    }
-    openGamePage();
-
-}
-
-function openGamePage(){
-   window.open("game.html?Level="+myLevel);
+function setLevel(level) {
+  window.location.href = `game.html?level=${level}`;
 }
